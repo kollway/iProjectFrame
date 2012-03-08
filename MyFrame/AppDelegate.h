b004f5f375b7b326076ba2064f2c787bb3b9adbb
sean zheng <sean@seans-Mac-mini.local>
2012-03-08 15:40:29 +0800

    Initial Commit

---
+++ MyFrame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFrame
+//
+//  Created by sean zheng on 3/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
